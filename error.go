1e90e67e68d79988f0fe03fe91f1cf7c6c93c510
Yao Adzaku <yao.adzaku@gmail.com>
2019-07-17 03:39:39 +0000

    handle specific Upvest HTTP error codes

--- error.go
+++ error.go
@@ -4,10 +4,25 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-// APIError represents an error response from the Paystack API server
-type APIError map[string]interface{}
+var errorTypes = map[int]string{
+	400: "invalid_request_error",
+	401: "authentication_error",
+	409: "duplicate_user",
+	500: "server_error",
+}
+
+// APIError represents an error response from the Upvest API server
+type APIError struct {
+	Type       string                 `json:"type,omitempty"`
+	Message    string                 `json:"message,omitempty"`
+	StatusCode int                    `json:"code,omitempty"`
+	Details    map[string]interface{} `json:"details,omitempty"`
+	URL        *url.URL               `json:"url,omitempty"`
+	Header     http.Header            `json:"header,omitempty"`
+}
 
 // APIError supports the error interface
 func (aerr *APIError) Error() string {
@@ -18,7 +33,19 @@ func (aerr *APIError) Error() string {
 func newAPIError(resp *http.Response) *APIError {
 	p, _ := ioutil.ReadAll(resp.Body)
 
-	var upvestErrorResp APIError
+	var upvestErrorResp map[string]interface{}
 	_ = json.Unmarshal(p, &upvestErrorResp)
-	return &upvestErrorResp
+
+	errorType := "server_error"
+	if err, ok := errorTypes[resp.StatusCode]; ok {
+		errorType = err
+	}
+
+	return &APIError{
+		Type:       errorType,
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Details:    upvestErrorResp,
+		URL:        resp.Request.URL,
+	}
 }
