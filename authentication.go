ebf1b9d84fa84e9c3ef34c74e9e8c91abf5de360
Yao Adzaku <yao.adzaku@gmail.com>
2020-02-22 16:58:11 -0500

    fix typo

--- authentication.go
+++ authentication.go
@@ -34,7 +34,7 @@ type AuthProvider interface {
 // OAuthResponse represents succesful OAuth response
 type OAuthResponse struct {
 	AccessToken  string `json:"access_token"`
-	ExpiresIn    string `json:"exxpires_in"`
+	ExpiresIn    string `json:"expires_in"`
 	TokenType    string `json:"token_type"`
 	Scope        string `json:"scope"`
 	RefreshToken string `json:"refresh_token"`
