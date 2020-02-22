2b7720620df33134565557314af2963e7e60becc
Yao Adzaku <yao.adzaku@gmail.com>
2020-02-22 17:49:53 -0500

    represent aouth expiry timespan as integer

--- authentication.go
+++ authentication.go
@@ -34,7 +34,7 @@ type AuthProvider interface {
 // OAuthResponse represents succesful OAuth response
 type OAuthResponse struct {
 	AccessToken  string `json:"access_token"`
-	ExpiresIn    string `json:"expires_in"`
+	ExpiresIn    int    `json:"expires_in"`
 	TokenType    string `json:"token_type"`
 	Scope        string `json:"scope"`
 	RefreshToken string `json:"refresh_token"`
